8c318f662e0de7fc886b321bf54e25385e721c18
Gunvant Sarpate <gunvantsr@gmail.com>
2022-06-07 16:44:17 +0530

    Added mongoose

--- boilerplates/index.js
+++ boilerplates/index.js
@@ -1,25 +1,42 @@
-const express = require("express");
-const app = express;
+const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-const PORT = "5000"  || process.env.PORT;
-const DB_URI = "{IMPORT HERE}";
+const app = express();
+const PORT = '5000' || process.env.PORT;
+const DB_URI = '{IMPORT HERE}';
 
-mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=>{
-        console.log("Database connected successfully!");
-    })
-    .catch((error)=>{
-        console.log(error);
-});
+mongoose
+  .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Database connected successfully!');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
+app.use('/health', (req, res) => {
+  const healthCheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: Date.now(),
+  };
+  try {
+    res.send(healthCheck);
+  } catch (error) {
+    healthCheck.message = error;
+    res.status(503), send();
+  }
+});
 
-app.listen(port, ()=> {
-    console.log(`app is listening on http://localhost:${port}..`);
-})
+app.listen(PORT, () => {
+  console.log(`app is listening on http://localhost:${PORT}..`);
+});
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 const inquirer = require('inquirer');
 const fs = require('fs');
-const fse = require("fs-extra");
+const fse = require('fs-extra');
 const path = require('path');
 const { join } = require('path');
 const currentDir = process.cwd();
+const chalk = require('chalk');
 
 const QUESTIONS = [
   {
@@ -13,37 +14,37 @@ const QUESTIONS = [
     message: 'Project name:',
     validate: function (input) {
       if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-      else return 'Project name may only include letters, numbers, underscores and hashes.';
-    }
-  }
-
+      else
+        return 'Project name may only include letters, numbers, underscores and hashes.';
+    },
+  },
 ];
 
+inquirer.prompt(QUESTIONS).then((answers) => {
+  const projectName = answers['project-name'];
+  const modelName = answers['model-name'];
+  const boilerplatePath = `${__dirname}/boilerplates`;
 
-inquirer.prompt(QUESTIONS)
-  .then(answers => {
-      const projectName = answers['project-name'];
-      const modelName = answers['model-name']
-      const boilerplatePath = `${__dirname}/boilerplates`;
+  fs.mkdirSync(`${currentDir}/${projectName}`);
+  fse.copySync(boilerplatePath, projectName);
+  createContent(projectName);
+});
 
-      fs.mkdirSync(`${currentDir}/${projectName}`);
-      fse.copySync(boilerplatePath, projectName);
-      createContent(projectName);
-    });
-
-
-    let createContent = (projectName) => {
-      let projectPath = path.join(currentDir, projectName);
-      let packageJson = path.join(projectPath, 'package.json');
-      let packageObj = fse.readJsonSync(packageJson);
-      packageObj.name = projectName;
-      fs.writeFileSync(packageJson, JSON.stringify(packageObj));
-      welcomeMsg(projectName);
-      
-    }
-
-
+let createContent = (projectName) => {
+  let projectPath = path.join(currentDir, projectName);
+  let packageJson = path.join(projectPath, 'package.json');
+  let packageObj = fse.readJsonSync(packageJson);
+  packageObj.name = projectName;
+  fs.writeFileSync(packageJson, JSON.stringify(packageObj));
+  welcomeMsg(projectName);
+};
 
 let welcomeMsg = (projectName) => {
-  console.log(`Hola! we had created all the files to get started for your ${projectName} 🚀🚀🚀`)
-}
+  console.log(
+    chalk.yellow(
+      `Yay! we have created all the files to get started for your ${projectName}! 🚀`
+    )
+  );
+  console.log(chalk.yellow(`cd ${projectName}`));
+  console.log(chalk.blue(`Happy Coding!! ❤️`));
+};
